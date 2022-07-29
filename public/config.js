15b440fca4ed50f30963e69662b9b2bc126b938a
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-07-29 22:28:50 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
+    'm788847464-d2d4864d4629c5b8d1d00aee',
     'm784490063-7b5da437e7f1e0d67613714d',
     'm784497419-de55aa09902ccb3ab22d548a',
     'm784496436-71a4bf7b1e3bdf7756be131b',

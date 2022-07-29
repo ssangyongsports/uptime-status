427cbf3bd407e2bf266afa503233ca665ced3dcc
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-07-29 22:50:00 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -11,8 +11,12 @@ window.Config = {
     'm792360523-5df83b0aeddb373dbad567e8',
     'm792360527-67aa7346b7da96a88f0bb54d',
     'm792360525-b546eed56cf469069ff1ac4c',
+    'm784488775-dd1ad84b209c05f8e185c33e',
+    'm784490063-7b5da437e7f1e0d67613714d',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
-
+ 
   // 日志天数
   CountDays: 90,
 

20354f3eb39899cd32432e6c6ea925e4ef1409bb
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-07-30 19:30:07 +0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -22,35 +22,10 @@ function App() {
             <UptimeRobot key={key} apikey={key} />
           ))}
         </div>
-        <div class="tweet" id="1113961806886785024"></div>
-
-
-<script
-  src="https://code.jquery.com/jquery-3.4.0.slim.min.js"
-  integrity="sha256-ZaXnYkHGqIhqTbJ6MB4l9Frs/r7U4jlx7ir8PJYBqbI="
-  crossorigin="anonymous"></script>
-<script src="http://platform.twitter.com/widgets.js"></script>
-<script>
-	var tweets = $(".tweet");
-	
-	$(tweets).each( function( t, tweet ) { 
-
-    var id = $(this).attr('id');
- 
-    twttr.widgets.createTweet(
-      id, tweet, 
-      {
-        conversation : 'none',    // or all
-        cards        : 'hidden',  // or visible 
-        linkColor    : '#cc0000', // default is blue
-        theme        : 'light'    // or dark
-      });
-
-    });
-</script>
-<div id='footer'>
-          <p>監控 <Link to='/' text='雙龍體育' /> 檢測頻率 5 分鐘</p>
-          <p>&copy; 2022 <Link to='https://www.ssangyongsports.eu.org/' text='雙龍體育' />, Version {Package.version}</p>
+<a class="twitter-timeline" href="https://twitter.com/ssangyongsport?ref_src=twsrc%5Etfw">Tweets by ssangyongsport</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <div id='footer'>
+          <p>監控 <Link to='' text='雙龍龍' /> 檢測平率 5 分鐘</p>
+          <p>&copy; 2022 <Link to='' text='雙龍體育' />, Version {Package.version}</p>
         </div>
       </div>
     </>
